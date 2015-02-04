15efeb799bb4be1d339d1a86e9fab923a4c4b4c2
Ryan Snyder <ryan.snyder3@gmail.com>
2015-02-03 19:51:35 -0500

    Started using Pyinotify.

--- watcher.py
+++ watcher.py
@@ -1,15 +1,13 @@
 import sys
 import shlex
-import pexpect
 import argparse
+import pyinotify
 import subprocess
 
 import logging
 import logging.handlers as handlers
 
 from datetime import datetime
-from StringIO import StringIO
-from csv import reader
 
 parser = argparse.ArgumentParser(description='Watches a directory for changes, then syncs those changes to another '
                                              'directory.')
@@ -27,8 +25,8 @@ group.add_argument('--quiet', help='Supresses STDOUT output.', action='store_tru
 
 args = vars(parser.parse_args())
 
-OLDDIR = args['source_directory']
-NEWDIR = args['target_directory']
+OLDDIR = args['source_directory'].rstrip('/')
+NEWDIR = args['target_directory'].rstrip('/')
 
 watcher = logging.getLogger()
 watcher.setLevel(logging.DEBUG)
@@ -50,44 +48,36 @@ if not args['quiet']:
     watcher.addHandler(stdout)
 
 
-
-
-
-watcher.info("Executing watcher: inotifywait -r " + OLDDIR + " --event moved_to,close_write -m -c")
-
-inotify = pexpect.spawn('inotifywait -r ' + OLDDIR + ' --event moved_to,close_write -m -c')
-
-inotify.expect('Setting up watches.  Beware: since -r was given, this may take a while!\r\n', timeout=None)
-watcher.info("Watcher is initializing.")
-inotify.expect('Watches established.\r\n', timeout=None)
 # Begin Watching for Files
 
 
-
-last = 'Initial'
-
 lasttime = datetime.now()
 watcher.info("Watches established.")
-while True:
-    inotify.expect('\r\n', timeout=None)
-    event = inotify.before
-    watcher.debug(event)
-    output = reader(StringIO(event)).next()  # This will work until I get pyinotify working.
-    watcher.debug(output)
-    name = output[0] + output[-1]
-    folder = output[0].replace(OLDDIR, NEWDIR)
-    delta = datetime.now() - lasttime
-    if (last not in name) or delta.seconds > 60:
-        watcher.info('Syncing change: {}'.format(name))
-        last = name
-        lasttime = datetime.now()
 
-        command = 'rsync -PvarH {} {}'.format(name, folder).replace("'", r"\'").replace("&", r"\&")
+wm = pyinotify.WatchManager()
+# noinspection PyUnresolvedReferences
+mask = pyinotify.IN_CLOSE_WRITE | pyinotify.IN_MOVED_TO
+
+
+class EventHandler(pyinotify.ProcessEvent):
+    def process_IN_CLOSE_WRITE(self, event):
+        watcher.debug(event)
+        to_folder = event.path.replace(OLDDIR, NEWDIR) + '/'
+        watcher.info('Syncing change: {}'.format(event.pathname))
+        command = 'rsync -PvarH {} {}'.format(event.pathname, to_folder).replace("'", r"\'").replace("&", r"\&")
         if not args['no_tmux']:
             command = 'tmux send-keys -t rsync "{}" c-m'.format(command.replace(' ', r' '))
 
         watcher.info('Running command: {}'.format(command))
 
         subprocess.Popen(shlex.split(command), stdin=None, stdout=None, stderr=None)
-    else:
-        watcher.debug('We just saw "{}, {} seconds ago, skipping."'.format(last, delta.seconds))
+
+
+    def process_IN_MOVED(self, event):
+        self.process_IN_CLOSE_WRITE(event)
+
+
+notifier = pyinotify.ThreadedNotifier(wm, EventHandler())
+notifier.start()
+
+wdd = wm.add_watch(OLDDIR, mask, rec=True)
